9a7e0a5a2398c54cace8ac45a8fbee262e88b89e
Paul Brickfeld <paulbrickfeld@gmail.com>
2012-11-29 15:23:54 +0100

    Removed the SSL link for the collage
    All traffic automatically goes over SSL, the explicit SSL URL is no
    longer needed.

--- lib/cinch/plugins/links.rb
+++ lib/cinch/plugins/links.rb
@@ -7,7 +7,7 @@ module Cinch
         :tumblr   => 'http://whatindie.tumblr.com/',
         :gallery  => 'http://indie-gallery.herokuapp.com/',
         :stats    => 'http://zhaan.org/ircstats/indie/',
-        :collage  => 'https://what.cd/collages.php?id=13264 or https://ssl.what.cd/collages.php?id=13264',
+        :collage  => 'https://what.cd/collages.php?id=13264',
         :facebook => 'https://www.facebook.com/indievidualradio',
         :twitter  => 'https://twitter.com/indievidualme',
       }
