09a5729fb1cb8403007550e2a0e675b99ca555df
Jip van Reijsen <jipvanreijsen@gmail.com>
2013-11-24 20:37:05 +0100

    Added error handling to Cinch::Plugins::Slang

--- lib/cinch/plugins/slang.rb
+++ lib/cinch/plugins/slang.rb
@@ -11,8 +11,16 @@ module Cinch
       match /slang (.+)/i, :method => :slang
 
       def slang(m, word)
-        definition = UrbanDictionary.define(word).entries.first.definition
-        m.reply definition.gsub("\r", '. ').gsub(/\s+/, " ")
+        results = UrbanDictionary.define(word)
+
+        if results.nil?
+          m.reply "No definition for \"#{word}\"."
+        else
+          definition       = results.entries.first.definition
+          clean_definition = definition.gsub("\r", '. ').gsub(/\s+/, ' ')
+
+          m.reply "#{word.capitalize}: #{clean_definition}" 
+        end
       end
     end
   end
