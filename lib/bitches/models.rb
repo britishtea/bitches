cbe2ceecf70f17582b3e96f01b803090f20f549a
Jip van Reijsen <jipvanreijsen@gmail.com>
2014-01-17 17:37:55 +0100

    Put all models under Bitches::Models namespace
    Recommendation is removed because the plugin will be removed as well

--- lib/bitches/models.rb
+++ lib/bitches/models.rb
@@ -1,6 +1,10 @@
+require 'data_mapper'
+
 require 'bitches/models/user'
 require 'bitches/models/bad_word'
-require 'bitches/models/recommendation'
 
-# Assume DataMapper is already required.
 DataMapper::Property::String.length(255)
+
+DataMapper.setup :default, ENV['DATABASE_URL']
+DataMapper.finalize
+DataMapper.auto_upgrade!
--- lib/bitches/models/bad_word.rb
+++ lib/bitches/models/bad_word.rb
@@ -1,10 +1,12 @@
-module Models
-  class Badword
-    include DataMapper::Resource
-  
-    storage_names[:default] = 'bad_words'
-  
-    property :id,   Serial
-    property :word, String, :required => true, :unique => true
+module Bitches
+  module Models
+    class Badword
+      include DataMapper::Resource
+    
+      storage_names[:default] = 'bad_words'
+    
+      property :id,   Serial
+      property :word, String, :required => true, :unique => true
+    end
   end
 end
