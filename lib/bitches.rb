24aacae71223bce3aca1ebcbaa69fedc0e276808
Jip van Reijsen <jipvanreijsen@gmail.com>
2014-01-27 18:50:34 +0100

    Fix up environment variables

--- lib/bitches.rb
+++ lib/bitches.rb
@@ -33,30 +33,36 @@ module Bitches
           c.authentication.level    = :h
           c.authentication.channel  = ENV["AUTH_CHANNEL"]
 
-          require "bitches/plugins/big_brother"
 
-          c.plugins.plugins << Bitches::Plugins::BigBrother
-          c.plugins.options[Bitches::Plugins::BigBrother] = {
-            :channel => ENV["MONITOR_CHANNEL"]
-          }
+          if ENV.key? "MONITOR_CHANNEL"
+            require "bitches/plugins/big_brother"
 
-
-          # require "cinch/plugins/identify"
-
-          # c.plugins.plugins << Cinch::Plugins::Identify
-          # c.plugins.options[Cinch::Plugins::Identify] = {
-          #   :password => ENV["NICKSERV_PASSWORD"] || "",
-          #   :type     => :nickserv
-          # }
+            c.plugins.plugins << Bitches::Plugins::BigBrother
+            c.plugins.options[Bitches::Plugins::BigBrother] = {
+              :channel => ENV["MONITOR_CHANNEL"]
+            }
+          end
 
 
-          require "bitches/plugins/lastfm"
+          if ENV.key? "NICKSERV_PASSWORD"
+            require "cinch/plugins/identify"
 
-          c.plugins.plugins << Bitches::Plugins::LastFM
-          c.plugins.options[Bitches::Plugins::LastFM] = {
-            :api_key    => ENV["LASTFM_KEY"],
-            :api_secret => ENV["LASTFM_SECRET"]
-          }
+            c.plugins.plugins << Cinch::Plugins::Identify
+            c.plugins.options[Cinch::Plugins::Identify] = {
+              :password => ENV["NICKSERV_PASSWORD"],
+              :type     => :nickserv
+            }
+          end
+
+          if ENV.key?("LASTFM_KEY") && ENV.key?("LASTFM_SECRET")
+            require "bitches/plugins/lastfm"
+
+            c.plugins.plugins << Bitches::Plugins::LastFM
+            c.plugins.options[Bitches::Plugins::LastFM] = {
+              :api_key    => ENV["LASTFM_KEY"],
+              :api_secret => ENV["LASTFM_SECRET"]
+            }
+          end
 
 
           require "bitches/plugins/links"
@@ -64,16 +70,18 @@ module Bitches
           c.plugins.plugins << Bitches::Plugins::Links
 
 
-          require "bitches/plugins/media"
+          if ENV.key?("MEDIA_URL") && ENV.key?("MEDIA_SECRET")
+            require "bitches/plugins/media"
 
-          c.plugins.plugins << Bitches::Plugins::Media
-          c.plugins.options[Bitches::Plugins::Media] = {
-            :url           => ENV["GALLERY_URL"],
-            :secret        => ENV["GALLERY_SECRET"],
-            :channels      => ENV["CHANNELS"].split(","),
-            :ignored_hosts => ["https://fbcdn-sphotos-c-a.akamaihd.net/"],
-            :ignored_tags  => ["nsfw", "nsfl", "ignore"]
-          }
+            c.plugins.plugins << Bitches::Plugins::Media
+            c.plugins.options[Bitches::Plugins::Media] = {
+              :url           => ENV["MEDIA_URL"],
+              :secret        => ENV["MEDIA_SECRET"],
+              :channels      => ENV["CHANNELS"].split(","),
+              :ignored_hosts => ["https://fbcdn-sphotos-c-a.akamaihd.net/"],
+              :ignored_tags  => ["nsfw", "nsfl", "ignore"]
+            }
+          end
 
 
           require "cinch/plugins/preview"
